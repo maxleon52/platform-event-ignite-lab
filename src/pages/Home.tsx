bba2d5f02ce9d55d277d58f9364401ba44688302
maxleon52 <maxleon522@gmail.com>
2022-06-24 21:21:38 -0300

    task: add css page Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,52 @@
+import { Logo } from "../components/Logo";
+
 export function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col items-center min-h-screen bg-blur bg-cover bg-no-repeat">
+      <div className="w-full max-w-[68.75rem] flex items-center justify-between mt-20 mx-auto">
+        <div className="max-w-[40rem]">
+          <Logo />
+          <h1 className="mt-8 text-[2.5rem] leading-tight">
+            Construa uma{" "}
+            <strong className="text-blue-500">aplicação completa</strong>, do
+            zero, <strong className="text-blue-500">com React</strong>
+          </h1>
+
+          <p className="mt-4 text-gray-200 leading-relaxed">
+            Em apenas uma semana você vai dominar na prática uma das tecnologias
+            mais utilizadas e com alta demanda para acessar as melhores
+            oportunidades do mercado.
+          </p>
+        </div>
+
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+          <strong className="text-2xl mb-6 block">
+            Inscreva-se gratuitamente
+          </strong>
+
+          <form className="flex flex-col gap-2 w-full">
+            <input
+              className="bg-gray-900 rounded px-5 h-14"
+              type="text"
+              placeholder="Seu nome completo"
+            />
+            <input
+              className="bg-gray-900 rounded px-5 h-14"
+              type="email"
+              placeholder="Digite seu e-mail"
+            />
+
+            <button
+              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-all"
+              type="submit"
+            >
+              Garantir minha vaga
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <img className="mt-10" src="/src/assets/code-mockup.png" alt="" />
+    </div>
+  );
 }
